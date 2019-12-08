decdd8b1795f82d92ac78882d8caf2a7d4f71ac8
Coduhuey <dalemu@ucsd.edu>
2019-12-07 18:46:09 -0800

    One more graph after this

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 //el = document.getElemenById("");
 //document.addEventListener OR el.on('click', functionName)
-
+/*
 var delayInMilliseconds = 500; // half a second
 
 setTimeout(function() { 
-
-   var iframe = document.getElementsByTagName('iframe');
+*/
+   /*var iframe = document.getElementsByTagName('iframe');
    for(var i = 0; i < iframe.length; i++){
     iframe[i].style.background = '#ff6633';
     iframe[i].contentWindow.document.body.style.backgroundColor = '#ff6633';
+   }*/
+/*
+   let frame = document.getElementsByClassName('handler')[0];
+   for(var i = 0; i < frame.length; i++){
+      frame[i].contentWindow.postMessage('#ff6633', 'http://127.0.0.1:5500/index.html:181:57');
    }
 
-}, delayInMilliseconds);
+}, delayInMilliseconds);*/
